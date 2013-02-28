9052346c5a9bd56bfb74776836ce7f111f7d4bfb
weicongju <weicongju@gmail.com>
2013-02-28 23:28:04 +0800

    south

---
+++ content/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Content'
+        db.create_table('content_content', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
+            ('body', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal('content', ['Content'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Content'
+        db.delete_table('content_content')
+
+
+    models = {
+        'content.content': {
+            'Meta': {'object_name': 'Content'},
+            'body': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['content']

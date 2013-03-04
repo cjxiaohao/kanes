6e73c4ab75455175f5fdcd345c80f36b101ab98b
weicongju <weicongju@gmail.com>
2013-03-04 21:21:23 +0800

    改名content_

--- content/urls.py
+++ content/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+    url(r'_/write/(?P<path>[\w\/]+)?$', 'content.views.write', name='content_write'),
     url(r'(?P<user>\w+)\/(?P<path>[\w+\/]+)?$', 'content.views.view', \
-        name='view'),
-    url(r'_/write/([\w\/]+)?$', 'content.views.write', name='write'),
+        name='content_view'),
 )

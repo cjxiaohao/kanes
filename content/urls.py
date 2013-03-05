f6e1b2267ec5dc28e8a04aee767674b45de6cc8c
weicongju <weicongju@gmail.com>
2013-03-05 11:26:21 +0800

    路径匹配点

--- content/urls.py
+++ content/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^_/$', 'content.views.all', name='content_all' ),
-    url(r'_/write/(?P<path>[\w\d\s\/]+)?$', 'content.views.write', name='content_write'),
-    url(r'^(?P<user>[\w\d]+)\/(?P<path>[\w\d\s]+)?$', 'content.views.view', \
+    url(r'_/write/(?P<path>[\w\d\s\.\/]+)?$', 'content.views.write', name='content_write'),
+    url(r'^(?P<user>[\w\d]+)\/(?P<path>[\w\d\s\.\/]+)?$', 'content.views.view', \
         name='content_view'),
 )

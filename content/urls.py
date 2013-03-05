25b12f0d344c683f2a5e9e3a31caa05286ec910a
weicongju <weicongju@gmail.com>
2013-03-05 11:08:05 +0800

    view的url

--- content/urls.py
+++ content/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^_/$', 'content.views.all', name='content_all' ),
     url(r'_/write/(?P<path>[\w\/]+)?$', 'content.views.write', name='content_write'),
-    url(r'(?P<user>\w+)\/(?P<path>[\w+\/]+)?$', 'content.views.view', \
+    url(r'^(?P<user>[\w\d]+)\/(?P<path>[\w\d]+)?$', 'content.views.view', \
         name='content_view'),
 )

5cd902fb513a8dbc2c27044eaa1a3324db733720
IMCJ <weicongju@gmail.com>
2013-03-07 00:09:03 +0800

    支持markdown

--- content/models.py
+++ content/models.py
@@ -1,3 +1,5 @@
+#! -*- encoding:utf-8 -*-
+
 from django.db import models
 from django.db.models.signals import pre_save
 from django.contrib.auth.models import User
@@ -5,10 +7,28 @@ from util import Revision
 from datetime import datetime
 from pdb import set_trace as bp
 
+import misaka
+import houdini
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+
+SYNTAX = ( \
+    ( "TEXT", "Text" ),
+    ( "HTML", "HTML" ),
+    ( "MARKDOWN", "Markdown" )
+)
+
 class Content ( models.Model ):
     user = models.ForeignKey ( User, editable = False, default = 0 )
-    slug = models.CharField ( max_length = 255, db_index = True, unique = True )
-    body = models.TextField ( )
+    slug = models.CharField ( u"路径", max_length = 255, db_index = True, \
+                              unique = True )
+    body = models.TextField ( u"内容" )
+    syntax = models.CharField ( u"语法解析器", max_length = 50, \
+                                choices = SYNTAX, default = "TEXT", \
+                                null = True, blank = True )
+    public = models.BooleanField ( u"公开", default = False )
+    html = models.TextField ( editable = False, default = "" )
     revisions = models.IntegerField ( editable = False, default = 0 )
     update_at = models.DateTimeField ( auto_now = True, auto_now_add = True, \
                                        editable = False, default = \
@@ -45,9 +65,26 @@ class Changelog ( models.Model ):
         return "user: %s, %s revision:%d" % ( self.content.user.username, \
                                     self.content.slug, self.revision )
 
+
+class BleepRenderer(misaka.HtmlRenderer, misaka.SmartyPants):
+    def block_code(self, text, lang):
+        if not lang:
+            return '\n<pre><code>%s</code></pre>\n' % \
+                houdini.escape_html(text.strip())
+        lexer = get_lexer_by_name(lang, stripall=True)
+        formatter = misaka.HtmlFormatter()
+        return highlight(text, lexer, formatter)
+
 def pre_save_content ( sender, **kwargs ):
     instance = kwargs['instance']
-
+    if "MARKDOWN" == instance.syntax:
+        renderer = BleepRenderer ( )
+        md = misaka.Markdown ( renderer, \
+                               extensions=misaka.EXT_FENCED_CODE | \
+                               misaka.EXT_NO_INTRA_EMPHASIS )
+        instance.html = md.render ( instance.body )
+    elif instance.syntax in ( "TEXT", "HTML" ):
+        instance.html = instance.body
     if instance.pk:
         instance.make_revision ( )
 

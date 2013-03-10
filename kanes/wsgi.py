e5453c06b266129e3a60bedb30b1eba2e3f0a10c
CJ <weicongju@gmail.com>
2013-03-10 16:40:28 +0800

    wsgi的包名还是gist2c

--- kanes/wsgi.py
+++ kanes/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gist2c.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kanes.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
